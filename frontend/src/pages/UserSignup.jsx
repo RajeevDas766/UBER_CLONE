bc4efd49b723dfaef675ef28fd2b1b6d69067825
RajeevDas766 <rajeevdas766@gmail.com>
2024-12-09 14:23:02 +0530

    Frontend

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -85,7 +85,7 @@ const UserSignup = () => {
       <button 
       className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base'  
       >
-      Login</button>
+      Create account</button>
 
       </form>
       <p className='text-center'>Already have a account? <Link to='/login' className='text-blue-600'>Login here</Link></p>
